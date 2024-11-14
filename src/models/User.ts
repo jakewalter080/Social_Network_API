b40469e9622436fbea1a32158f135d518be98b9c
Jake Walter <jakewalter080@gmail.com>
2024-11-13 20:48:03 -0600

    updated  User.Schema

--- src/models/User.ts
+++ src/models/User.ts
@@ -13,17 +13,24 @@ const UserSchema = new Schema<IUser>(
         type: String,
         required: true,
         unique: true,
-        match : /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/, 'Please fill a valid email address'],
-        },
-        thoughts: [
-            {
-                type: Schema.Types.ObjectId,
-                ref: 'Thought',
-            },
-            ],
-        },
-        {
-          toJSON: {
+        match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please enter a valid email address'],
+      },
+    
+    thoughts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'Thought',
+      },
+    ],
+    friends: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+      },
+      ],
+    },
+    {
+        toJSON: {
             virtuals: true,
             getters: true,
           },
