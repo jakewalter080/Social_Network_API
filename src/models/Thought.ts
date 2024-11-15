714e992067c27cf33927c8a582c6c2b74e39093d
Jake Walter <jakewalter080@gmail.com>
2024-11-14 19:23:14 -0600

    add Thoughtschema

--- src/models/Thought.ts
+++ src/models/Thought.ts
@@ -30,3 +30,30 @@ const ReactionSchema = new Schema<IReaction>(
         id: false,
     }
     );
+    const ThoughtSchema = new Schema<IThought>(
+        {
+          thoughtText: {
+            type: String,
+            required: true,
+            minlength: 1,
+            maxlength: 280,
+          },
+        createdAt: {
+          type: Date,
+          default: Date.now,
+          get: (timestamp: Date) => formatDate(timestamp),
+        },
+        username: {
+            type: String,
+            required: true,
+          },
+          reactions: [ReactionSchema],
+        },
+        {
+          toJSON: {
+            virtuals: true,
+            getters: true,
+          },
+          id: false,
+        }
+      );
