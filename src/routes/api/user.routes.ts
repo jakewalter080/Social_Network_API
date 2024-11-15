79f31267d07e0763bf99c212585307577937f956
Jake Walter <jakewalter080@gmail.com>
2024-11-14 20:25:54 -0600

    updated user.routes

--- src/routes/api/user.routes.ts
+++ src/routes/api/user.routes.ts
@@ -5,18 +5,83 @@ const router = Router();
 
 router
   .route('/')
-  .get(userController.getAllUsers)
-  .post(userController.createUser);
+  .get(async (req, res, next) => {
+    try {
+      await userController.getAllUsers(req, res);
+    } catch (err) {
+      next(err);
+    }
+  })
+  .post(async (req, res, next) => {
+    try {
+      await userController.createUser(req, res);
+    } catch (err) {
+      next(err);
+    }
+  });
 
 router
   .route('/:id')
-  .get(userController.getUserById)
-  .put(userController.updateUser)
-  .delete(userController.deleteUser);
+  .get(async (req, res, next) => {
+    try {
+      await userController.getUserById(req, res);
+    } catch (err) {
+      next(err);
+    }
+  })
+  .put(async (req, res, next) => {
+    try {
+      await userController.updateUser(req, res);
+    } catch (err) {
+      next(err);
+    }
+  })
+  .delete(async (req, res, next) => {
+    try {
+      await userController.deleteUser(req, res);
+    } catch (err) {
+      next(err);
+    }
+  });
 
 router
   .route('/:userId/friends/:friendId')
-  .post(userController.addFriend)
-  .delete(userController.removeFriend);
+  .post(async (req, res, next) => {
+    try {
+      await userController.addFriend(req, res);
+    } catch (err) {
+      next(err);
+    }
+  })
+  .delete(async (req, res, next) => {
+    try {
+      await userController.removeFriend(req, res);
+    } catch (err) {
+      next(err);
+    }
+  });
 
-export default router
+export default router;
+
+// import { Router } from 'express';
+// import { userController } from '../../controllers/user.controllers';
+
+// const router = Router();
+
+// router
+//   .route('/')
+//   .get(userController.getAllUsers)
+//   .post(userController.createUser);
+
+// router
+//   .route('/:id')
+//   .get(userController.getUserById)
+//   .put(userController.updateUser)
+//   .delete(userController.deleteUser);
+
+// router
+//   .route('/:userId/friends/:friendId')
+//   .post(userController.addFriend)
+//   .delete(userController.removeFriend);
+
+// export default router
