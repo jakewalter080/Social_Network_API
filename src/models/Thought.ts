5eb0202ad455c42ae5c5b8f2a1eed1309fa08e88
Jake Walter <jakewalter080@gmail.com>
2024-11-14 19:17:25 -0600

    add Reactionschema

---
+++ src/models/Thought.ts
@@ -0,0 +1,32 @@
+import { Schema, model, Types } from 'mongoose';
+import { IThought, IReaction } from '../types';
+import { formatDate } from '../utils/dateFormat';
+
+const ReactionSchema = new Schema<IReaction>(
+    {
+      reactionId: {
+        type: Schema.Types.ObjectId,
+        default: () => new Types.ObjectId(),
+      },
+        reactionBody: {
+        type: String,
+        required: true,
+        maxlength: 280,
+        },
+        username: {
+        type: String,
+        required: true,
+        },
+        createdAt: {
+        type: Date,
+        default: Date.now,
+        get: (timestamp: Date) => formatDate(timestamp),
+        },
+    },
+    {
+        toJSON: {
+        getters: true,
+        },
+        id: false,
+    }
+    );
