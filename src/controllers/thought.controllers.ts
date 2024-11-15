17fb9f85151205b14e689cbc9ffce72395ac833d
Jake Walter <jakewalter080@gmail.com>
2024-11-14 19:35:01 -0600

    define thought contorller

--- src/controllers/thought.controllers.ts
+++ src/controllers/thought.controllers.ts
@@ -2,3 +2,13 @@ import { Request, Response } from 'express';
 import { Thought, User } from '../models';
 import { handleError } from '../utils/errorHandler';
 
+export const thoughtController = {
+    // Get all thoughts
+    async getAllThoughts(_req: Request, res: Response) {
+      try {
+        const thoughts = await Thought.find().sort({ createdAt: -1 });
+        return res.json(thoughts);
+      } catch (err) {
+        return handleError(res, err);
+      }
+    },
